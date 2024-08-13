b127650cadecb12499994cc87227bfb18923b00c
kkot1204 <kimggotbom@gmail.com>
2024-08-13 17:16:29 +0900

    html css 수정

--- src/App.jsx
+++ src/App.jsx
@@ -64,37 +64,56 @@ function App() {
 
   return (
     <div className="container">
-      <h1>2024 파리 올림픽 메달 트래커</h1>
+      <h1>🏅2024 파리 올림픽🏅</h1>
       <form className="input-group" onSubmit={onSubmit}>
-        <input
-          type="text"
-          name="country"
-          value={countryName}
-          onChange={(event) => setCountryName(event.target.value)}
-        />
-        <input
-          type="number"
-          name="gold"
-          value={gold}
-          onChange={(event) => setGold(event.target.value)}
-        />
-        <input
-          type="number"
-          name="silver"
-          value={silver}
-          onChange={(event) => setSilver(event.target.value)}
-        />
-        <input
-          type="number"
-          name="bronze"
-          value={bronze}
-          onChange={(event) => setBronze(event.target.value)}
-        />
-        <button type="submit">국가 추가</button>
-        <button type="button" onClick={onUpdate}>
-          업데이트
-        </button>
+        <div class="input-field">
+          <label>🌎국가명</label>
+          <input
+            type="text"
+            placeholder="국가 입력"
+            name="country"
+            value={countryName}
+            onChange={(event) => setCountryName(event.target.value)}
+          />
+        </div>
+        <div class="input-field">
+          <label>🥇금메달</label>
+          <input
+            type="number"
+            name="gold"
+            value={gold}
+            onChange={(event) => setGold(event.target.value)}
+          />
+        </div>
+        <div class="input-field">
+          <label>🥈은메달</label>
+          <input
+            type="number"
+            name="silver"
+            value={silver}
+            onChange={(event) => setSilver(event.target.value)}
+          />
+        </div>
+        <div class="input-field">
+          <label>🥉동메달</label>
+          <input
+            type="number"
+            name="bronze"
+            value={bronze}
+            onChange={(event) => setBronze(event.target.value)}
+          />
+        </div>
+        <div class="button-group">
+          <button type="submit">국가 추가</button>
+          <button type="button" onClick={onUpdate}>
+            업데이트
+          </button>
+        </div>
       </form>
+      <div>
+        <p>✨아직 추가된 국가가 없습니다. 메달을 추적하세요!</p>
+      </div>
+
       <div>
         {countries.map((country) => (
           <div key={country.countryName}>
