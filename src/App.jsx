b05f4ffd4cf5a67490ed8607633973b85daac4e3
kkot1204 <kimggotbom@gmail.com>
2024-08-13 11:25:53 +0900

    ui 레이아웃 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 function App() {
-  return <div>hello world</div>;
+  return (
+    <div className="container">
+      <h1>2024 파리 올림픽 메달 트래커</h1>
+      <form className="input-group"></form>
+      <div></div>
+    </div>
+  );
 }
 
 export default App;
